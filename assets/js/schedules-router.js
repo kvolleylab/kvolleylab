8b07da1d36a43d667695e2604f69bc5999e2cd9c
kvolleylab <kvolleylab.team@gmail.com>
2026-08-14 21:10:39 +0900

    Route schedule renderer by competition

---
+++ assets/js/schedules-router.js
@@ -0,0 +1,10 @@
+(()=>{
+  const params=new URLSearchParams(location.search);
+  const competition=params.get('competition')||'vnl';
+  const script=document.createElement('script');
+  script.async=false;
+  script.src=competition==='avc-men-cup'
+    ? 'assets/js/schedules-avc.js?v=20260814-1'
+    : 'assets/js/schedules-v3.js?v=20260715-15';
+  document.head.appendChild(script);
+})();
