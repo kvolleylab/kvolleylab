443eaac3bbc434854c3d48b778ac7efa6536e160
kvolleylab <kvolleylab.team@gmail.com>
2026-08-16 18:07:15 +0900

    Bump AVC schedule renderer cache version

--- assets/js/schedules-router.js
+++ assets/js/schedules-router.js
@@ -4,7 +4,7 @@
   const script=document.createElement('script');
   script.async=false;
   script.src=competition==='avc-men-cup'
-    ? 'assets/js/schedules-avc.js?v=20260814-1'
+    ? 'assets/js/schedules-avc.js?v=20260816-2'
     : 'assets/js/schedules-v3.js?v=20260715-15';
   document.head.appendChild(script);
 })();
