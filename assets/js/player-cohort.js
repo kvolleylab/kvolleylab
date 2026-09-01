8dbc74abcf5f65bdec86cf92022285c36e372fba
kvolleylab <kvolleylab.team@gmail.com>
2026-09-02 00:43:32 +0900

    data: restore Korea 2010 U18 final roster (#143)
    * data: restore Korea 2010 U18 final roster
    
    * feat: load 2010 U18 restored roster
    
    * feat: include 2010 U18 in cohort view

--- assets/js/player-cohort.js
+++ assets/js/player-cohort.js
@@ -1,6 +1,6 @@
 (() => {
   const DATA_URL='data/national/national_team_history_v1.json';
-  const LEGACY_URLS=['data/national/national_team_history_2018_2019.json','data/national/national_team_history_2016_2017.json','data/national/national_team_history_2014_2015.json','data/national/national_team_roster_2014_u18.json','data/national/national_team_history_2012_2013.json','data/national/national_team_history_2010_2011.json','data/national/national_team_world_u23_2015.json'];
+  const LEGACY_URLS=['data/national/national_team_history_2018_2019.json','data/national/national_team_history_2016_2017.json','data/national/national_team_history_2014_2015.json','data/national/national_team_roster_2014_u18.json','data/national/national_team_history_2012_2013.json','data/national/national_team_history_2010_2011.json','data/national/national_team_world_u23_2015.json','data/national/national_team_roster_2010_u18.json'];
   const LINK_URL='data/national/player_id_links_v1.json';
   const params=new URLSearchParams(location.search),initialYear=Number(params.get('birth_year'))||2005,levels=['U16','U17','U18','U19','U20','U21','U23'];
   const grid=document.getElementById('cohortGrid'),title=document.getElementById('cohortTitle'),summary=document.getElementById('cohortSummary'),filter=document.getElementById('birthYearFilter');
