a69108170ff0efd8ea2e6bd813b538129e397e26
kvolleylab <kvolleylab.team@gmail.com>
2026-09-14 23:02:13 +0900

    Fix roster runtime escaping after league-country flag sync

--- assets/js/avc-men-roster-runtime.js
+++ assets/js/avc-men-roster-runtime.js
@@ -6,7 +6,7 @@
 
   const DATA='data/competitions/avc-men-continental-2026-rosters.json?v=20260904-1';
   const CLUBS='data/competitions/avc-men-continental-2026-clubs-2026-27.json?v=20260914-3';
-  const esc=v=>String(v??'').replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot',"'":'&#39;'}[c]));
+  const esc=v=>String(v??'').replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
   const posOrder={S:1,OP:2,OH:3,MB:4,L:5,U:6,'':9};
   const fmtDob=v=>{if(!v)return '생년월일 미확인';const [y,m,d]=String(v).split('-');return y&&m&&d?`${y}.${m}.${d}`:v};
   const flag=t=>`https://flagcdn.com/w80/${t.flag}.png`;
